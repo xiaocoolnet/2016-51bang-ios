fc1d2451a1f37e7297303b4743d33907edb33a0e
ac M <hemingyang@imac.local>
2016-10-09 13:49:22 +0800

    bug

--- 51Bang_ios_2016/AffirmOrderViewController.swift
+++ 51Bang_ios_2016/AffirmOrderViewController.swift
@@ -65,7 +65,7 @@ class AffirmOrderViewController: UIViewController,UITableViewDelegate,UITableVie
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 60
+        return 55
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == 2 {
@@ -80,7 +80,7 @@ class AffirmOrderViewController: UIViewController,UITableViewDelegate,UITableVie
     }
     
     func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-        return 10
+        return 8
     }
     
     func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
